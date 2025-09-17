0de0ae302c0038cd48b9c8f5acd139e5bdd3315b
César Freitas <costacesar014@gmail.com>
2025-09-16 22:18:04 -0300

    16.09

--- faculdade/html_em_python/main.py
+++ faculdade/html_em_python/main.py
@@ -1,1 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DOM123'
+
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login')
+def login():
+
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+
+    return redirect('/')
